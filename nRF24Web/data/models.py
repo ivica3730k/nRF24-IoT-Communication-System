4216412e6a1711fcbcc1bdcb020e30377da11854
ivica <ivica-matic@outlook.com>
2019-06-03 16:26:31 +0100

    Changes in Django Web Part
    Updated id to uint32

--- nRF24Web/data/models.py
+++ nRF24Web/data/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 # Create your models here.
 class dataField(models.Model):
-    fieldId = models.TextField(primary_key=True, default= hex(int(uuid.uuid4().hex[:6],16)), editable=False,unique = True)
+    fieldId = models.TextField(primary_key=True, default= hex(int(uuid.uuid4().hex[:8],16)), editable=False,unique = True)
 
 class dataContent(models.Model):
     field = models.ForeignKey(dataField, on_delete=models.CASCADE)
