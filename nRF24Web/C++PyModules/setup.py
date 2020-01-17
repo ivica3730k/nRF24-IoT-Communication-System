efb42a2aa980560a6125c82d6e5cc56c1381ccce
IcE <ivica-matic@outlook.com>
2020-01-17 23:51:38 +0000

    Reorganised folder structure, deleted uneccesary C and C++ modules

--- nRF24Web/C++PyModules/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
- 
-module = Extension('myModule', sources = ['myModule.cpp'])
- 
-setup (name = 'PackageName',
-        version = '1.0',
-        description = 'This is a package for myModule',
-        ext_modules = [module])
